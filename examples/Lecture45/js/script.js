bfdc70bd32de51c145d268f1092297c2f92e26d2
LeighDudenhoeffer <69485246+LeighDudenhoeffer@users.noreply.github.com>
2021-03-18 17:23:23 -0600

    class code for js class today

--- examples/Lecture45/js/script.js
+++ examples/Lecture45/js/script.js
@@ -1,22 +1,26 @@
 // Object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "blue";
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "blue";
 
-console.log(company);
-console.log("Company CEO name is: " 
-  + company.ceo.firstName);
+// console.log(company);
+// console.log("Company CEO name is: "
+// + company.ceo.firstName);
 
-console.log(company["name"]);
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// console.log(company["name"]);
+// var stockPropName = "stock of company";
+// company [stockPropName] = 110;
 
-console.log("Stock price is: " + 
-  company[stockPropName]);
+// console.log("Stock price is: " + company[stockPropName]);
+// var stockPropName = "stock of company";
+// company[stockPropName] = 110;
 
-// Better way: object literal
+// console.log("Stock price is: " +
+//   company[stockPropName]);
+
+// // Better way: object literal
 var facebook = {
   name: "Facebook",
   ceo: {
@@ -26,7 +30,7 @@ var facebook = {
   "stock of company": 110
 };
 
-console.log(facebook.ceo.firstName);
+console.log(facebook);
 
 
 
--- examples/Lecture46/js/script.js
+++ examples/Lecture46/js/script.js
@@ -1,44 +1,44 @@
-// Functions are First-Class Data Types
-// Functions ARE objects
-function multiply(x, y) {
-  return x * y;
-}
-multiply.version = "v.1.0.0";
-console.log(multiply.version);
+// // Functions are First-Class Data Types
+// // Functions ARE objects
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// multiply.version = "v.1.0.0";
+// console.log(multiply.version);
 
 
-// Function factory
+// // Function factory
+// function makeMultiplier(multiplier) {
+//   var myFunc = function (x) {
+//      return multiplier * x;
+//     };
+//     return myFunc;
+// }
+
+// var multiplyBy3 = makeMultiplier(3);
+// console.log(multiplyBy3(12));
+// var doubleAll = makeMultiplier(2);
+// console.log(doubleAll(1000));
+
+// // Passing functions as arguments
+// function doOperationOn(x, operation) {
+//   return operation(x);
+// }
+
+// var result = doOperationOn(5, multiplyBy3);
+// console.log(result);
+// result = doOperationOn(1000, doubleAll);
+// console.log(result);
+
 function makeMultiplier(multiplier) {
-  var myFunc = function (x) {
+  var myFunFunc = function (x) {
     return multiplier * x;
   };
-
-  return myFunc;
+  return myFunFunc;
 }
 
-var multiplyBy3 = makeMultiplier(3);
-console.log(multiplyBy3(10));
-var doubleAll = makeMultiplier(2);
-console.log(doubleAll(100));
-
-
-
-// Passing functions as arguments
-function doOperationOn(x, operation) {
-  return operation(x);
-}
-
-var result = doOperationOn(5, multiplyBy3);
-console.log(result);
-result = doOperationOn(100, doubleAll);
-console.log(result);
-
-
-
-
-
-
-
-
+var operation = makeMultiplier (10);
+console.log(operation (10));
 
 
